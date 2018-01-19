07aa7ba6b27a1d4a45d93d7d3c46ead24bb15739
Tom Spencer <tomspencer_uk@yahoo.co.uk>
2018-01-19 19:06:38 +0000

    Completes chapter 3

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+
+  def full_title(page_name=nil)
+    return "Ruby on Rails Tutorial Sample App" if page_name.nil? || page_name == ""
+    # return  "Home | Ruby on Rails Tutorial Sample App" if page_name == "Home"
+    page_name + " | Ruby on Rails Tutorial Sample App"
+  end
 end
